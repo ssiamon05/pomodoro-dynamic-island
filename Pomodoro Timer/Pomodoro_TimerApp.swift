f89a888014dc9d9abee7efd187f4276731ac36b4
Sam Siamon <sam.siamon@neudesic.com>
2023-12-05 12:40:25 -0800

    Initial Commit

---
+++ Pomodoro Timer/Pomodoro_TimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pomodoro_TimerApp.swift
+//  Pomodoro Timer
+//
+//  Created by Sam.Siamon on 12/5/23.
+//
+
+import SwiftUI
+
+@main
+struct Pomodoro_TimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
